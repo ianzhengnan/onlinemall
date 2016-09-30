4ec6a13e40cced254c5bef54b7716869a75a09ce
Ian Zheng <ian.zheng@sap.com>
2016-09-30 18:26:19 +0800

    switch to cookie for login
    Change-Id: I7ae56f8891fa1bbfe13af08d55f82dec1f28ce77

--- onlinemall/src/main/java/com/ian/onlinemall/controller/LoginController.java
+++ onlinemall/src/main/java/com/ian/onlinemall/controller/LoginController.java
@@ -1,16 +1,22 @@
 package com.ian.onlinemall.controller;
 
-import java.util.HashMap;
-import java.util.Map;
+//import java.util.HashMap;
+//import java.util.Map;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.struts2.convention.annotation.Result;
 import org.apache.struts2.convention.annotation.Results;
+import org.apache.struts2.interceptor.ServletRequestAware;
+import org.apache.struts2.interceptor.ServletResponseAware;
 import org.apache.struts2.rest.DefaultHttpHeaders;
 import org.apache.struts2.rest.HttpHeaders;
 
 import com.ian.onlinemall.domain.User;
 import com.ian.onlinemall.exception.OmException;
-import com.opensymphony.xwork2.ActionContext;
+//import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ModelDriven;
 
 @Results({
@@ -18,16 +24,18 @@ import com.opensymphony.xwork2.ModelDriven;
 	@Result(name="error", location="/WEB-INF/content/test/error.jsp"),
 	@Result(name="index", location="/WEB-INF/content/test/login.jsp")
 })
-public class LoginController extends BaseController implements ModelDriven<User>{
+public class LoginController extends BaseController implements ModelDriven<User>, ServletResponseAware, ServletRequestAware{
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -6475078107590624578L;
 	private User user;
-	private Map<String, Object> userId = new HashMap<String, Object>();
+//	private Map<String, Object> userId = new HashMap<String, Object>();
 	private User loginUser;
-	private ActionContext act;
+//	private ActionContext act;
+	private HttpServletResponse response;
+	private HttpServletRequest request;
 
 	public User getUser() {
 		return user;
@@ -39,10 +47,14 @@ public class LoginController extends BaseController implements ModelDriven<User>
 	
 	// GET /login
 	public HttpHeaders index(){
-		act = ActionContext.getContext();
-		if(act.getSession().size() > 0 && act.getSession().get("loginUser") != null){
+//		act = ActionContext.getContext();
+		String key = request.getCookies()[0].getName();
+		if(key != null && key == "loginUsername"){
 			setTip("您已经登陆！");
-			user = (User) act.getSession().get("loginUser");
+			user = new User();
+			user.setName(request.getCookies()[0].getValue());
+			
+//			user = (User) act.getSession().get("loginUser");
 			return new DefaultHttpHeaders("success")
 					.disableCaching();
 		}else{
@@ -61,14 +73,19 @@ public class LoginController extends BaseController implements ModelDriven<User>
 	public String checkUser() throws OmException{
 		
 		User user = getUser();
-		act = ActionContext.getContext();
+//		act = ActionContext.getContext();
 				
 		loginUser = userMgr.signInByName(user.getName(), user.getPassword());
 		
 		if (user != null && loginUser != null) {
 			setTip("登录成功！");
-			userId.put("loginUser", loginUser);
-			act.getSession().put("loginUser", loginUser);
+//			userId.put("loginUser", loginUser);
+//			act.getSession().put("loginUser", loginUser);
+			
+			Cookie cookie = new Cookie("loginUsername", user.getName());
+			cookie.setMaxAge(60 * 60);
+			response.addCookie(cookie);
+			
 			return "success";
 		}else{
 			setTip("登录失败！");
@@ -78,6 +95,14 @@ public class LoginController extends BaseController implements ModelDriven<User>
 
 	public User getModel() {
 		return user;
+	}
+
+	public void setServletResponse(HttpServletResponse paramHttpServletResponse) {
+		this.response = paramHttpServletResponse;
+	}
+
+	public void setServletRequest(HttpServletRequest paramHttpServletRequest) {
+		this.request = paramHttpServletRequest;
 	}
 
 }
--- onlinemall/src/main/java/com/ian/onlinemall/controller/LogoutController.java
+++ onlinemall/src/main/java/com/ian/onlinemall/controller/LogoutController.java
@@ -1,20 +1,25 @@
 package com.ian.onlinemall.controller;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.struts2.convention.annotation.Result;
 import org.apache.struts2.convention.annotation.Results;
-import org.apache.struts2.rest.RestActionSupport;
+import org.apache.struts2.interceptor.ServletRequestAware;
 
-import com.opensymphony.xwork2.ActionContext;
+//import com.opensymphony.xwork2.ActionContext;
 
 @Results({
 	@Result(name="success", type="redirectAction", params = {"actionName" , "login"})
 })
-public class LogoutController extends RestActionSupport{
+public class LogoutController extends BaseController implements ServletRequestAware{
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -5031149294672562432L;
+	
+	private HttpServletRequest request;
 	
 	// GET /api/logout
 	public String index(){
@@ -22,9 +27,21 @@ public class LogoutController extends RestActionSupport{
 	}
 	
 	public String logout(){
-		ActionContext atx = ActionContext.getContext();
-		atx.getSession().remove("loginUser");
+//		ActionContext atx = ActionContext.getContext();
+//		atx.getSession().remove("loginUser");
+		
+		request.getCookies()[0].getName();
+		
+		for (Cookie cookie : request.getCookies()) {
+			if (cookie.getName() == "loginUsername") {
+				cookie.setMaxAge(0);
+			}
+		}
 		return "success";
+	}
+
+	public void setServletRequest(HttpServletRequest paramHttpServletRequest) {
+		this.request = paramHttpServletRequest;
 	}
 	
 }
