e9b3b4a5cef5f643d1ebfbd2a27d418cd7676f06
Ian Zheng <ian.zheng@sap.com>
2016-10-28 16:40:23 +0800

    update interceptor1

--- onlinemall/src/main/java/com/ian/onlinemall/interceptors/AuthInterceptor.java
+++ onlinemall/src/main/java/com/ian/onlinemall/interceptors/AuthInterceptor.java
@@ -26,8 +26,6 @@ public class AuthInterceptor implements Interceptor{
     public String intercept(ActionInvocation actionInvocation) throws Exception {
 
 //        ActionContext atx = actionInvocation.getInvocationContext();
-
-
         request = ServletActionContext.getRequest();
         Cookie cie = null;
         for (Cookie cookie: request.getCookies()) {
